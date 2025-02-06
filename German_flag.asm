3e3b3caf9cff5282415a506ce8f1897f4ae774f4
Hilt <143846331+SenjorHilter@users.noreply.github.com>
2025-02-06 22:30:55 +0000

    Create German_flag.asm

---
+++ German_flag.asm
@@ -0,0 +1,55 @@
+.MODEL TINY 
+.STACK 100h 
+.CODE 
+begin:     
+    mov ah, 0h     
+    mov al, 12h     
+    int 10h      
+
+    mov ah, 0ch     
+    mov bh, 0     
+    mov cx, 0     
+
+row_alteration:     
+    mov dx, 0     
+    mov al, 0
+    
+print_black_line:
+    int 10h             
+    inc dx             
+    cmp dx, 160             
+    jne print_black_line
+    
+    mov al, 15
+    
+print_white_line:
+    int 10h
+    inc dx
+    cmp dx, 320
+    jne print_white_line
+    
+    mov al, 4
+    
+print_red_line:
+    int 10h 
+    inc dx
+    cmp dx, 480
+    jne print_red_line
+    
+    inc cx             
+    cmp cx, 640             
+    jne row_alteration          
+
+    mov al, 15     
+    mov cx, 0
+    
+    mov ah, 01h      
+    int 21h      
+    
+    mov ax, 3      
+    int 10h       
+ 
+    mov ax, 4c00h      
+    int 21h
+
+END begin
